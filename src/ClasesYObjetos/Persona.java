8ab77835999a6e503e9802ee28e72735c137209f
Alejandro Romero <alex_100000000@hotmail.com>
2022-01-12 14:32:20 +0100

    toma de contacto clases y  objetos

---
+++ src/ClasesYObjetos/Persona.java
@@ -0,0 +1,19 @@
+package ClasesYObjetos;
+
+public class Persona {
+
+	private String nombre;
+	
+	private int edad;
+	
+	private double estatura;
+	
+	public void setNombre (String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+}
---
+++ src/ClasesYObjetos/ProgramaPersona.java
@@ -0,0 +1,15 @@
+package ClasesYObjetos;
+
+public class ProgramaPersona {
+
+	public static void main(String[] args) {
+		
+		Persona p1 = new Persona();
+		
+		p1.setNombre("Juanito");
+		
+		System.out.println(p1.getNombre());
+	
+	}
+
+}
