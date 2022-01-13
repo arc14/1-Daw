ac65d212622bc9d5594453276a97907ee92d6a1b
Alejandro Romero <alex_100000000@hotmail.com>
2022-01-13 09:21:18 +0100

    clase 13/01

--- src/ClasesYObjetos/Persona.java
+++ src/ClasesYObjetos/Persona.java
@@ -16,4 +16,30 @@ public class Persona {
 		return nombre;
 	}
 	
+	public void setEdad (int edad) throws Exception{
+		if (edad <= 0) {
+			throw new Exception("No puede ser menor o igual a 0");
+		} else {
+			this.edad = edad;
+		}
+		
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setEstatura(double estatura) {
+		if (estatura <= 0 ) {
+			System.out.println("Estatura no permitida");
+		} else {
+			this.estatura = estatura;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "Persona [nombre= " + nombre + ", edad= " + edad + ", estatura= " + estatura + "]";
+	}
+	
 }
--- src/ClasesYObjetos/ProgramaPersona.java
+++ src/ClasesYObjetos/ProgramaPersona.java
@@ -1,15 +1,63 @@
 package ClasesYObjetos;
 
+import java.util.Scanner;
+
 public class ProgramaPersona {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		//persona 1
 		Persona p1 = new Persona();
 		
-		p1.setNombre("Juanito");
+		System.out.println("Dime el nombre de la persona 1");
+		p1.setNombre(sc.next());
 		
-		System.out.println(p1.getNombre());
+		System.out.println("Dime la edad para la persona 1");
+		try {
+			p1.setEdad(sc.nextInt());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Dime la estatura de la persona 1");
+		p1.setEstatura(sc.nextDouble());
+		
+		//Persona 2
+		
+		Persona p2 = new Persona();
+		
+		System.out.println("Dime el nombre de la persona 2");
+		p2.setNombre(sc.next());
+		
+		System.out.println("Dime la edad para la persona 2");
+		try {
+			p2.setEdad(sc.nextInt());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Dime la estatura de la persona 2");
+		p2.setEstatura(sc.nextDouble());
+		
+		
+		if (p1.getEdad() > p2.getEdad()) {
+			System.out.println(p1);
+			System.out.println(p2);
+		
+		} else if(p1.getEdad() < p2.getEdad()){
+			System.out.println(p2);
+			System.out.println(p1);
+			
+		} else {
+			System.out.println("No se pueden ordenar, tienen la misma edad");
+		}
 	
+		sc.close();
+		
 	}
 
 }
